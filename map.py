70ddb59615a82f9174ffa3191d8cc35755250fda
KingGranger <cheeridon@gmail.com>
2018-07-16 13:34:13 -0400

    first commit

---
+++ map.py
@@ -0,0 +1,26 @@
+import folium
+import requests
+import json
+from datetime import datetime
+import pdb
+
+
+earthquakes = requests.get("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2018-01-01&minlatitude=22&maxlatitude=50&minlongitude=-83&maxlongitude=-63&orderby=magnitude").json()['features']
+map = folium.Map(location=[37.50, -73], zoom_start=5, prefer_canvas=True, tiles="Mapbox Bright")
+fg = folium.FeatureGroup(name="vacation")
+
+for earthquake in earthquakes:
+    # pdb.set_trace()
+    date = datetime.fromtimestamp(earthquake['properties']['time']/1000).strftime('%Y-%m-%d %H:%M:%S')
+    place = earthquake['properties']['place']
+    mag = earthquake['properties']['mag']
+    if mag > 2:
+        color = 'orange'
+    else:
+        color = 'green'
+    fg.add_child(folium.CircleMarker(location=[earthquake['geometry']['coordinates'][1],earthquake['geometry']['coordinates'][0]], radius=5, popup="Date: %s \n Place: %s \n Magnitude: %s" %(date, place, mag), fill_color=color, color= 'grey', fill=True, fill_opacity=0.7))
+
+# fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+
+map.add_child(fg)
+map.save('index.html')
